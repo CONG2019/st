be3fca4ba3706353c33964b5eca0b030762d2a5b
jacken <1178187720@qq.com>
2022-06-04 06:55:31 +0000

    Add cyclefonts and set font size to 24pix

--- config.def.h
+++ config.def.h
@@ -5,7 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *fonts[] = {
+  "Liberation Mono:pixelsize=24:antialias=true:autohint=true",
+  "tewi:pixelsize=24:antialias=true:autohint=true",
+  "inconsolata:pixelsize=24:antialias=true:autohint=true"
+};
+static int fonts_current = 0;
+
 static int borderpx = 2;
 
 /*
@@ -204,6 +210,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_S,           cyclefonts,     {}        },
 };
 
 /*
